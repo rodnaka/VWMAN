1f1472150952eec32f2ba73127993deadfd086a3
Administrador <rodrigonaka@msn.com>
2016-06-10 16:16:35 -0300

    Template Inicial

---
+++ VWMAN/VWMAN/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VWMAN
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
